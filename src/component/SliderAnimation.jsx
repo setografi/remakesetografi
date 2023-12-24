9dcbc3caf84200dbc8651ca389992f7d2db58224
setografi <hitonaashbell@gmail.com>
2023-12-24 17:59:17 +0700

    update slider4

--- src/component/SliderAnimation.jsx
+++ src/component/SliderAnimation.jsx
@@ -3,72 +3,72 @@ import React from "react";
 const Slider = () => {
   const cards = [
     {
-      img: "./src/assets/skill/adobephotoshop-color.svg",
+      img: "../assets/skill/adobephotoshop-color.svg",
       alt: "Image 1",
       text: "Adobe Photoshop",
     },
     {
-      img: "./src/assets/skill/adobeillustrator-color.svg",
+      img: "../assets/skill/adobeillustrator-color.svg",
       alt: "Image 2",
       text: "Adobe Illustrator",
     },
     {
-      img: "./src/assets/skill/coreldraw-color.svg",
+      img: "../assets/skill/coreldraw-color.svg",
       alt: "Image 3",
       text: "CorelDraw",
     },
     {
-      img: "./src/assets/skill/canva-color.svg",
+      img: "../assets/skill/canva-color.svg",
       alt: "Image 4",
       text: "Canva",
     },
     {
-      img: "./src/assets/skill/figma-color.svg",
+      img: "../assets/skill/figma-color.svg",
       alt: "Image 5",
       text: "Figma",
     },
     {
-      img: "./src/assets/skill/blender-color.svg",
+      img: "../assets/skill/blender-color.svg",
       alt: "Image 6",
       text: "Blender",
     },
     {
-      img: "./src/assets/skill/html5-color.svg",
+      img: "../assets/skill/html5-color.svg",
       alt: "Image 7",
       text: "HTML",
     },
     {
-      img: "./src/assets/skill/css3-color.svg",
+      img: "../assets/skill/css3-color.svg",
       alt: "Image 8",
       text: "CSS",
     },
     {
-      img: "./src/assets/skill/javascript-color.svg",
+      img: "../assets/skill/javascript-color.svg",
       alt: "Image 9",
       text: "JavaScript",
     },
     {
-      img: "./src/assets/skill/bootstrap-color.svg",
+      img: "../assets/skill/bootstrap-color.svg",
       alt: "Image 10",
       text: "Bootstrap",
     },
     {
-      img: "./src/assets/skill/react-color.svg",
+      img: "../assets/skill/react-color.svg",
       alt: "Image 11",
       text: "ReactJS",
     },
     {
-      img: "./src/assets/skill/tailwindcss-color.svg",
+      img: "../assets/skill/tailwindcss-color.svg",
       alt: "Image 12",
       text: "Tailwind CSS",
     },
     {
-      img: "./src/assets/skill/elixir-color.svg",
+      img: "../assets/skill/elixir-color.svg",
       alt: "Image 13",
       text: "Elixir",
     },
     {
-      img: "./src/assets/skill/phoenixframework-color.svg",
+      img: "../assets/skill/phoenixframework-color.svg",
       alt: "Image 14",
       text: "Phoenix Framework",
     },
